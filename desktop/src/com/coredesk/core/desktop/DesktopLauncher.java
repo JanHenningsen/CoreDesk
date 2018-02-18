063dc9b2e67e2eae6467cf61e260ca837bba0ad6
janhe <jan.henningsen@gmx.net>
2018-02-18 12:13:29 +0100

    Libgdx core

---
+++ desktop/src/com/coredesk/core/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.coredesk.core.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.coredesk.core.CoreDesk;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoreDesk(), config);
+	}
+}
